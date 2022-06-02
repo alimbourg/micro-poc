61f7260f07f79da20327ab3dc037aa2e2a3a2efd
Awen Limbourg <alimbourg@gmail.com>
2022-06-02 19:17:10 +0200

    [consul] and java

---
+++ krackend.sh
@@ -0,0 +1,8 @@
+docker run -d --name krackend \
+    -p 8000:8000 \
+    -p 8090:8090 \
+    devopsfaith/krakend:2.0.4
+
+docker run -p 8080:8080 -v $PWD:/etc/krakend/ devopsfaith/krakend run --config /etc/krakend/krakend.json
+go run hello.go
+go run world.go
